0b3361b2eda1ea5ff12262a41262f797b6d8a08f
yash kumar mishra <mishrayash3778@gmail.com>
2024-08-24 19:22:28 +0530

    kjk

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiFillInstagram, AiFillYoutube, AiFillLinkedin } from "react-icons/ai";
+import { FaGitSquare } from "react-icons/fa";
+import { Context } from "../../main";
+
+const Footer = () => {
+  const isDashboard = useLocation("http://localhost:5173/dashboard");
+  const { mode, setMode } = useContext(Context);
+
+  return (
+    <footer
+      className={
+        isDashboard.pathname === "/dashboard"
+          ? "hideFooter"
+          : mode === "light"
+          ? "light-footer"
+          : "dark-footer"
+      }
+    >
+      <div className="container">
+        <div className="logo">ZuPay <span>BLOG</span></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
